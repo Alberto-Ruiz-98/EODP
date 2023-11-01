b9a52474b30103c68b1a0fcf59dfdab5e302835c
Alberto-Ruiz-98 <100485642@alumnos.uc3m.es>
2023-11-01 01:20:57 +0100

    UPDATE

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,8 +5,8 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\ALBERTO\EODP\Test2\auxiliary'
-indir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-ISM\input\gradient_alt100_act150' # small scene
-outdir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-ISM\outputsAlberto'
+indir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-E2E\sgm_out' # small scene
+outdir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-E2E\outputsAlberto'
 
 
 # Initialise the ISM
--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,8 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\ALBERTO\EODP\Test2\auxiliary'
-indir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-L1B\input'
-outdir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-L1B\outputsAlberto'
+indir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-E2E\outputsAlberto'
+outdir = r'C:\ALBERTO\EODP\Test2\EODP_TER\EODP-TS-E2E\outputsAlbertoL1b'
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
